115419b049f388a75d654159438a1deebdc8638d
DESKTOP-8VTPQ9M\kch_a <dhidalgovalencia@yahoo.es>
2022-06-17 15:25:59 -0500

    update jenkisnfile

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -33,6 +33,7 @@ pipeline {
             junit 'results/*_result.xml'
         }
         failure {  
-             mail bcc: '', body: "<b>Example</b><br>Project: ${env.JOB_NAME} <br>Build Number: ${env.BUILD_NUMBER} <br> URL de build: ${env.BUILD_URL}", cc: '', charset: 'UTF-8', from: '', mimeType: 'text/html', replyTo: '', subject: "ERROR CI: Project name -> ${env.JOB_NAME}", to: "daniel.hidalgov@epn.edu.ec";  
+             mail bcc: '', body: "<b>Example</b><br>Project: ${env.JOB_NAME} <br>Build Number: ${env.BUILD_NUMBER} <br> URL de build: ${env.BUILD_URL}", cc: '', charset: 'UTF-8', from: '', mimeType: 'text/html', replyTo: '', subject: "ERROR CI: Project name -> ${env.JOB_NAME}", to: "daniel.hidalgov@epn.edu.ec";
+        }     
     }
 }
