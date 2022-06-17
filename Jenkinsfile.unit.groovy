aadea11dc1e9f77c17e757308819f89c4e0a3449
DESKTOP-8VTPQ9M\kch_a <dhidalgovalencia@yahoo.es>
2022-06-16 21:56:46 -0500

    update jenkisfile

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -15,6 +15,12 @@ pipeline {
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
+        stage('API tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
     }
     post {
         always {
